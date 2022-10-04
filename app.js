00f646bc13414ee583afce10c9d2dd802a38c220
metal-oopa <sudip5banerjee1974@gmail.com>
2022-10-05 03:57:07 +0530

    add line numbers to formatted json

--- app.js
+++ app.js
@@ -6,6 +6,15 @@ const minify = document.querySelector(".minify");
 prettify.addEventListener("click", () => {
   try {
     let formatted = JSON.stringify(JSON.parse(input.value), null, 4);
+
+    let line = 1;
+    const totalLines = formatted.match(/\n/g).length;
+    const padLength = 5 + Math.floor(totalLines / 10);
+
+    formatted = formatted.replace(/^/gm, () =>
+      String(line++).padEnd(padLength)
+    );
+
     output.value = formatted;
   } catch (err) {
     output.value = err.message;
@@ -13,10 +22,10 @@ prettify.addEventListener("click", () => {
 });
 
 minify.addEventListener("click", () => {
-    try {
-      let formatted = JSON.stringify(JSON.parse(input.value));
-      output.value = formatted;
-    } catch (err) {
-      output.value = err.message;
-    }
-  });
+  try {
+    let formatted = JSON.stringify(JSON.parse(input.value));
+    output.value = formatted;
+  } catch (err) {
+    output.value = err.message;
+  }
+});
